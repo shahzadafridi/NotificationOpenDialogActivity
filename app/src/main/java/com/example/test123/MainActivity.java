b76ce652bfb10e1a81ddf7f7a1fb20641090098e
Nguyen Ba Hung <hung@bio-rithm.com>
2023-07-05 17:32:30 +0700

    Migrate app using androidx and update lib

--- app/src/main/java/com/example/test123/MainActivity.java
+++ app/src/main/java/com/example/test123/MainActivity.java
@@ -9,8 +9,8 @@ import android.graphics.Color;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
-import android.support.v4.app.NotificationCompat;
-import android.support.v7.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
